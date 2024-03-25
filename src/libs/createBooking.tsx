4c919e68e731082e83aaf8f2c4e150d69aa2a181
DecQmenT <earn5060@gmail.com>
2024-03-25 23:18:44 +0700

    add create booking

---
+++ src/libs/createBooking.tsx
@@ -0,0 +1,18 @@
+export default async function createBooking(token:string, bookDate:string) {
+    const response = await fetch(`https://presentation-day-1-nongearnproject-black.vercel.app/api/v1/bookings`, {
+        method: "POST",
+        headers: {
+            authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            bookDate: bookDate,
+        }),
+    })
+    if(!response.ok) {
+        throw new Error("Cannot create coworking space")
+    }
+
+    const responseData = await response.json();
+
+    return responseData;
+}
