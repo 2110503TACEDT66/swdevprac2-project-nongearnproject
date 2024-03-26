3f809d4337fc0f8979723da219c090e960a822c5
focustoplaner <focustoplaner@hotmail.com>
2024-03-26 10:50:08 +0700

    fix welcome

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,12 +3,16 @@ import Image from 'next/image'
 import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useSession } from 'next-auth/react'
+import getUserProfile from '@/libs/getUserProfile'
 
 export default function Banner() {
     const covers = ['/img/cover.jpg', '/img/cover2.jpg', '/img/cover3.jpg', '/img/cover4.jpg']
     const [index, setIndex] = useState(0);
 
     const router = useRouter(); 
+
+    const { data: session } = useSession()
+    console.log(session?.user.name)
 
     return (
         <div className='block p-1 m-0 w-screen relative h-[80vh]' onClick={()=>{setIndex(index + 1)}}>
@@ -23,11 +27,11 @@ export default function Banner() {
             </div>
 
             {
-                    session ? <div className='z-30 absolute top-5 right-10
-                    font-semibold text-cyan-800 text-xl'>
-                            Welcome {session.user?.name}
-                            </div>
-                            : null
+                session ? <div className='z-30 absolute top-5 right-10
+                font-semibold text-cyan-800 text-xl'>
+                        Welcome {session.user.name}
+                        </div>
+                        : null
             }
 
             <button className='bg-white text-cyan border border-cyan-600
