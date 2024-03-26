d49715da22f4dffd929da8c2a9d52692bec9500b
DecQmenT <earn5060@gmail.com>
2024-03-26 22:36:21 +0700

    -

--- src/components/TopMenuItem.tsx
+++ src/components/TopMenuItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function TopMenuItem({title, pageRef} : 
 {title:string, pageRef:string}) {
     return (
-        <Link href={pageRef} className='w-auto mx-6 text-center mt-auto mb-auto text-lg'>
+        <Link href={pageRef} className='w-auto ml-12 text-center mt-auto mb-auto text-lg'>
             {title}
         </Link>
     );
